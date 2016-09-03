89ac524ca91b52a1d7a2f19cafce3a52725a1ae2
xutl <79109681@qq.com>
2016-09-03 12:31:03 +0800

    update

---
+++ BaiduApiStore.php
@@ -0,0 +1,73 @@
+<?php
+namespace xutl\api;
+
+use Yii;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+use yii\httpclient\Client;
+
+/**
+ * Class BaiduApiStore
+ * @property string $baseUrl
+ * @property string $apiKey
+ * @package xutl\api
+ */
+class BaiduApiStore extends Component
+{
+    public $baseUrl = 'http://apis.baidu.com/';
+
+    /**
+     * @var string 百度ApiKey
+     */
+    public $apiKey;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if (empty ($this->apiKey)) {
+            throw new InvalidConfigException ('The "apiKey" property must be set.');
+        }
+    }
+
+    /**
+     * 请求Api接口
+     * @param string $url
+     * @param string $method
+     * @param array $params
+     * @param array $headers
+     * @return \yii\httpclient\Response
+     */
+    private function api($url, $method, array $params, array $headers = [])
+    {
+        $client = new Client([
+            'baseUrl' => $this->baseUrl,
+            'responseConfig' => [
+                'format' => Client::FORMAT_JSON
+            ],
+        ]);
+        $request = $client->createRequest();
+        if (!empty($headers)) {
+            $request->setHeaders($headers);
+        }
+        $request->addHeaders(['apikey' => $this->apiKey])
+            ->setMethod($method)
+            ->setData($params)
+            ->setUrl($url);
+        return $request->send();
+    }
+
+    /**
+     * get mobile
+     *
+     * @param string mobile number
+     * @return array
+     */
+    public function getMobile($mobile)
+    {
+        $response = $this->api('apistore/mobilenumber/mobilenumber', 'GET', ['phone' => $mobile]);
+        return $response;
+    }
+}
